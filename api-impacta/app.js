eae99532bd4629358eb3361914928275e77eaffe
PriK <priscillakleine@gmail.com>
2023-09-04 20:00:02 -0300

    iniciada conectacao BD

--- api-impacta/app.js
+++ api-impacta/app.js
@@ -6,6 +6,21 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'DESKTOP-0EFMBNU\SQLEXPRESS',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'sa2', //update me
+                password: 'sa2'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'Canecas'
+
 let createItems = [];
 let idCounter = 1;
 
@@ -33,3 +48,5 @@ app.get('/buscar', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
