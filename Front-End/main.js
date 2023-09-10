0effe8f6091cc11ef45117a9068ad921775b8579
PriK <priscillakleine@gmail.com>
2023-09-10 17:51:23 -0300

    alterações no front para conexao com BD

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,18 +1,18 @@
 
 
         function cadastrarItem() {
-            var codigo = document.getElementById("codigo").value;
+            // var codigo = document.getElementById("codigo").value;
             var nomedoProduto = document.getElementById("nomeProduto").value;
             var descricao = document.getElementById("descricao").value;
-            var quantidade = document.getElementById("quantidade").value;
-            var precoUnitario = document.getElementById("precoUnitario").value;
-            console.log(codigo);
+            var quantidade = parseInt(document.getElementById("quantidade").value);
+            var precoUnitario = parseFloat(document.getElementById("precoUnitario").value);
+            // console.log(codigo);
             console.log(nomedoProduto);
             console.log(descricao);
             console.log(quantidade);
             console.log(precoUnitario);
             
-            var itemData = {Codigo: codigo, NomedoProduto: nomedoProduto, Descricao: descricao, Quantidade: quantidade, PrecoUnitario: precoUnitario};
+            var itemData = {NomedoProduto: nomedoProduto, Descricao: descricao, Quantidade: quantidade, PrecoUnitario: precoUnitario, Total: 0};
             
             console.log(itemData);
 
